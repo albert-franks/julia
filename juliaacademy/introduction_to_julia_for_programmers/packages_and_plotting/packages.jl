a3ac91d18ecf9444738695a867ed578b7eca43da
Alberto Ciacci <131367497+albert-franks@users.noreply.github.com>
2023-06-27 21:58:47 +0100

    adding programs that perform basic packages import operations.

---
+++ juliaacademy/introduction_to_julia_for_programmers/packages_and_plotting/packages.jl
@@ -0,0 +1,43 @@
+#=
+juliaacademy.com - Introduction to Julia (for programmers)
+Chapter: Packages and Plotting - packages
+Link: https://juliaacademy.com/courses/375479/lectures/5863424
+Aim: to replicate and expand the contents of this tutorial
+=#
+
+#=
+
+A comprehensive repository of Julia packages is available here
+
+https://julialang.org/packages/
+
+To add a package to the current environment, we invoke using Pkg and then the add() 
+command with sole argument the name of the package.
+In this first example, we add the package "Example"
+=#
+using Pkg
+Pkg.add("Example")
+#=
+To use a package, we use the 'using' keyword followed by the name of the package
+Note: In Python, a package is added via 'pip install' and invoked in the program through the keyword 'import'.
+      In Julia, 'pip install' is replaced by Pkg.add() and 'import' is replaced by 'using'
+Note: There is an even more compact way to add package when using Julia from the command shell.
+      julia> "]" <- this opens the package manager
+      julia> add package_name <- this adds a package
+
+The source code for the Example package can be found here:
+
+https://julialang.github.io/Example.jl/stable/
+=#
+using Example
+# We try the hello() function from 'Example', which print an user-define message after 'Hello,'
+hello("my name is Mark.")
+
+#=
+We do the same for the package Colors
+
+https://juliagraphics.github.io/Colors.jl/stable/
+
+=#
+Pkg.add("Colors")
+using Colors
